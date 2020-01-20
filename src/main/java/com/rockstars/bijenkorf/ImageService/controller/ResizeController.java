66f09d58f0a261b7057ba864809b1c47c2ecb13a
Kevin van Mastrigt <kevinvanmastrigt@gmail.com>
2020-01-20 10:23:47 +0100

    Controller can now load images from resources (image/original)

--- src/main/java/com/rockstars/bijenkorf/ImageService/controller/ResizeController.java
+++ src/main/java/com/rockstars/bijenkorf/ImageService/controller/ResizeController.java
@@ -1,19 +1,51 @@
 package com.rockstars.bijenkorf.ImageService.controller;
 
+import com.rockstars.bijenkorf.ImageService.service.ImageProviderService;
 import com.rockstars.bijenkorf.ImageService.service.ResizeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RestController("/image")
 public class ResizeController {
 
     @Autowired
     ResizeService resizeService;
 
-    @GetMapping("/test/{name}")
-    public String getTest(@PathVariable() String name) {
-        return resizeService.test(name);
+    @Autowired
+    ImageProviderService imageProviderService;
+
+    @GetMapping(value = {"/show/{typeName}/{seoName}", "/show/{typeName}"})
+    public ResponseEntity<byte[]> getImage(@PathVariable() String typeName, @PathVariable(required = false) String seoName, @RequestParam() String reference) {
+        BufferedImage image;
+        try {
+            image = imageProviderService.loadOriginalImage(reference);
+        } catch(IOException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ImageIO.write( image, "jpg", outputStream );
+            outputStream.flush();
+            byte[] imageBytes = outputStream.toByteArray();
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(imageBytes);
+
+        } catch(Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
--- src/main/java/com/rockstars/bijenkorf/ImageService/service/ImageProviderService.java
+++ src/main/java/com/rockstars/bijenkorf/ImageService/service/ImageProviderService.java
@@ -1,8 +1,17 @@
 package com.rockstars.bijenkorf.ImageService.service;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 @Service
 public class ImageProviderService {
 
+    public BufferedImage loadOriginalImage(String fileName) throws IOException {
+        var imgResource = new ClassPathResource("image/original/" + fileName);
+        return ImageIO.read(imgResource.getInputStream());
+    }
 }
